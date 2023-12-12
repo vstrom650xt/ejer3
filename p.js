8fe808dc17c33fcaa507661db9916d0f6da5386f
vstrom60xt <cerveramonzo@gmail.com>
2023-12-12 20:23:20 +0100

    11

--- p.js
+++ p.js
@@ -1,10 +1,15 @@
 const button = document.querySelector("button");
 
-function action(){
-    var input = document.getElementById("input");
-     var valor = input.value
-    const element = document.createTextNode( valor + " vas a palmar "); 
-    console.log(valor)
+function action() {
+    var nombreInput = document.getElementById("nombreInput");
+    var apellido1Input = document.getElementById("apellido1Input");
+    var apellido2Input = document.getElementById("apellido2Input");
 
-};
+    var nombre = nombreInput.value;
+    var apellido1 = apellido1Input.value;
+    var apellido2 = apellido2Input.value;
+
+    var resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.textContent = nombre + " " + apellido1 + " " + apellido2 + " vas a palmar ";
+}
 button.addEventListener("click", action)
